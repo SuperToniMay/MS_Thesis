efac29795065d6f4a14cb7481c05c89e549e997b
Toni Proescholdt <tonip2@hotmail.com>
2021-02-21 17:34:40 -0700

    Added plans in Discrete Choice code & practice pushing to GitHub

--- Discrete_Choice/Code/Discrete_Choice.R
+++ Discrete_Choice/Code/Discrete_Choice.R
@@ -2,3 +2,7 @@
 
 BRPop <- read.csv("Raw_Data/BRPOP_v02.csv")
 head(BRPop)
+
+#Subset BRPOP for what I will actually use to make it smaller
+#Add in code from previous attempts, fix sources and outputs
+#Edit from what Kezia said to do.
